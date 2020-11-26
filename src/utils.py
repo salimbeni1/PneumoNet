cb3b3aeed5a77785c059ab1cc0611af7be1e2a71
Luka <lukachinchaladze@gmai.com>
2020-11-26 13:13:25 +0100

    Stack_pos
    function for stacking all positios together.

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import keras.backend as K
 import sound_processing as sp
 from keras.utils import to_categorical
 import matplotlib.pyplot as plt
+from scipy.stats import mode
 
 
 def plot_history(history , title='model accuracy / loss'):
@@ -64,3 +65,47 @@ def prepare_samples( feat , cont , posi , pnbs , shuffle=True ,extractor = lambd
     train_position_big = train_position_big[shuffle_indices]
 
   return x_train, y_train, train_patientnb_big, train_position_big
+
+
+def stack_pos(patients, features, controls, positions, patientnbs):
+    x = []
+    y = []
+    for patient in np.unique(patients):
+        indx = patientnbs == patient
+
+        x_data, y_data, train_patientnb_big, train_position_big = prepare_samples(features[indx], controls[indx],
+                                                                              positions[indx], patientnbs[indx],
+                                                                              shuffle=False, extractor=lambda s:
+        sp.features_extraction(s, 22050, 'stft'))
+
+        shape = x_data[0].shape
+        freq = mode(train_position_big)  # returns the position with the most crops and the number of crops
+        for i in range(0, freq[1][0]):  # for each patient we make the x number of data points where x is the frequency of the most reoccurring position
+            sample = np.empty(shape)  # initialize empty array
+        for p in range(1, 9):  # iterate through positions
+            pos = 'P' + str(p)
+            if pos != freq[0][0]:  # if position is not the most reoccurring position
+                indx = np.argwhere(train_position_big == pos).flatten()  # return the indices of these positions
+                try:  # if position is present , stack
+                    try:  # append available crops first
+                        sample = np.dstack([sample, x_data[indx[i]]])
+                    except:  # once we run out of them take any random one
+                        sample = np.dstack([sample, x_data[
+                        np.random.choice(indx)]])  # randomly take any available crops for this position and stack depthwise
+                except:  # if position not available fill the layer with zeros
+                    sample = np.dstack([sample, np.zeros(shape)])
+            else:  # if the position is the most reoccuring one
+                indx = np.argwhere(
+                train_position_big == 'P' + str(p)).flatten()  # return the indicies of the most reocurring body position
+                sample = np.dstack([sample, x_data[indx[i]]])  # stack that body pos
+                y.append(controls[indx][0] == 'Ca')  # append the label of the patient
+            sample = np.delete(sample, 0, axis=2)  # delete the first row since it was used for initializing
+            x.append(sample)  # append into train array
+    x = np.array(x)
+    y = to_categorical(np.array(y).astype(int))
+    shuffle_indices = np.random.permutation(np.arange(y.shape[0]))
+
+    return x[shuffle_indices], y[shuffle_indices]
+
+
+
